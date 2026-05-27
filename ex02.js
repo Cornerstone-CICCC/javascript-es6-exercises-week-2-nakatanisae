8e3dc283564045e99e90bbc32716742ae5be9b63
Sae Nakatani <nakatanisae0218@gmail.com>
2026-05-26 18:13:21 -0700

    Exercises 2 completed

--- ex02.js
+++ ex02.js
@@ -2,12 +2,24 @@
 We'll be adding only the numbers in the array which match the given condition.
 
 Instruction
-Create a function named conditionalSum that will be given an array of numbers and a condition, in this case odd or even. Given this condition, add up only the values which match that condition. If no values match the condition, return 0.
+Create a function named conditionalSum that will be given an array of numbers and a condition, 
+in this case odd or even. Given this condition, add up only the values which match that condition.
+If no values match the condition, return 0.
 
 */
 
 const conditionalSum = function (values, condition) {
   // Your code here
+  if (condition === 'even' ){
+    const evenNum1 = values.filter((num) => num % 2 === 0  )
+    const evenNum2 = evenNum1.reduce((total, num) => total + num, 0)
+    return evenNum2
+  }  else {
+    const oddNum1 = values.filter((num) => num % 2 !== 0 )
+    const oddNum2 = oddNum1.reduce((total, num) => total + num, 0)
+    return oddNum2
+  }
+    
 };
 
 console.log(conditionalSum([1, 2, 3, 4, 5], "even")); // 6
