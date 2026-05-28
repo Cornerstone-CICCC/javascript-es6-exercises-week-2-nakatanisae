7e063e58cf2e9bb741f84b0426b7644500cfcc69
Sae Nakatani <nakatanisae0218@gmail.com>
2026-05-28 16:13:07 -0700

    Exercise 9 completed

--- ex09.js
+++ ex09.js
@@ -2,7 +2,8 @@
 In this exercise, we will be converting a normal string into camelCase text.
 
 Case Maker
-We will receive a normal string of words separated with spaces as the input. Our job is to convert these strings into camel cased strings.
+We will receive a normal string of words separated with spaces as the input. 
+Our job is to convert these strings into camel cased strings.
 
 Instruction
 Create a function named camelCase that will convert a string to camel case, and return the result.
@@ -10,7 +11,21 @@ Create a function named camelCase that will convert a string to camel case, and 
 
 const camelCase = function (input) {
   // Your code here
-};
+  const stringToArray1 = (input) => {
+    return input.split('')
+  }
+  const searchSpace = stringToArray1(input)
+
+  searchSpace.forEach((element,i) => {
+    if(element === ' '){
+      searchSpace[i + 1] = searchSpace[i + 1].toUpperCase()
+    }
+  });
+  const result = searchSpace.join("").replaceAll(" ","")
+
+  return result
+}
+
 
 console.log(camelCase("this is a string")); // thisIsAString
 console.log(camelCase("loopy cornerstone")); //loopyCornerstone
