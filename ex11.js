02bf3bcc8466a9744207d53271fe146b6f0bc547
Sae Nakatani <nakatanisae0218@gmail.com>
2026-05-28 17:56:29 -0700

    Exercise 11 completed

--- ex11.js
+++ ex11.js
@@ -1,27 +1,42 @@
 /*
-There's a new attraction at this year's Codeville festival. The organizers have decided to bring in several inflatable attractions, but they have no clue how to much blow them up. Each attraction needs to be pumped to a precise volume to achieve maximum festival fun!
+There's a new attraction at this year's Codeville festival. 
+The organizers have decided to bring in several inflatable attractions, 
+but they have no clue how to much blow them up. 
+Each attraction needs to be pumped to a precise volume to achieve maximum festival fun!
 
-The attractions are each made up of a combination of several different shapes: cones, spheres and prisms. For example, the giant inflatable duck is made up of two spheres (the body and head) and a cone (the beak) 🦆.
+The attractions are each made up of a combination of several different shapes: 
+cones, spheres and prisms. 
+For example, the giant inflatable duck is made up of two spheres (the body and head) and 
+a cone (the beak) 🦆.
 
-Each shape has a different calculation for determining volume, so we'll need to create a few functions that will help us figure out the volume of the various inflatable attractions.
+Each shape has a different calculation for determining volume, 
+so we'll need to create a few functions that will help us figure out 
+the volume of the various inflatable attractions.
 
 In this challenge, we'll need to implement four functions.
 
 The first three will calculate the volume of the individual shapes:
 
 sphereVolume()will calculate the volume of a sphere given a radius
+sphereVolume() は、与えられた半径から球の体積を計算します。
 coneVolume() will calculate the volume of a cone given a radius and a height
+coneVolume() は、与えられた半径と高さから円錐の体積を計算します。
 prismVolume() will calculate the volume of a prism given a height, a width, and a depth
+coneVolume() は、与えられた半径と高さから円錐の体積を計算します。
 Note
 Don't worry about getting the answers to the perfect precision, just check to see that you're close.
 
 Note
 We can find the formulas for calculating the volume of a sphere, cone, and prism online.
 
-The fourth function, totalVolume(), will receive an array containing the different shapes that make up a single attraction. The totalVolume function should use the previous three functions to calculate the total volume of an attraction.
+The fourth function, totalVolume(), will receive an array 
+containing the different shapes that make up a single attraction. 
+The totalVolume function should use the previous three functions to 
+calculate the total volume of an attraction.
 
 Instruction
-Implement the functions one by one. The example inputs and outputs below will help you check that each function is correct.
+Implement the functions one by one. 
+The example inputs and outputs below will help you check that each function is correct.
 */
 
 // Use the value below whenever you need the value of Pi
@@ -29,24 +44,38 @@ const PI = 3.14159;
 
 const sphereVolume = function (radius) {
   // Code here!
+  return (4 / 3)* Math.PI*(radius)*(radius)*(radius)
 };
 
 console.log(4186 < sphereVolume(10) && sphereVolume(10) < 4189); //true
 
 const coneVolume = function (radius, height) {
   // And here!
+  return (1 / 3)* Math.PI *(radius)*(radius)*(height)
 };
 
 console.log(45 < coneVolume(3, 5) && coneVolume(3, 5) < 49); //true
 
 const prismVolume = function (height, width, depth) {
   // Probably here too!
+  return (height)*(width)*(depth)
 };
 
 console.log(prismVolume(3, 4, 5) === 60); //true
 
 const totalVolume = function (solids) {
   // Code here? Yup!
+  let total = 0
+  solids.forEach((solid) =>{
+    if(solid.type === 'sphere'){
+      total = total + sphereVolume(solid.radius);
+    } else if(solid.type === 'cone'){
+      total = total + coneVolume(solid.radius, solid.height)
+    } else if(solid.type === 'prism'){
+      total = total + prismVolume(solid.height, solid.width, solid.depth);
+    }
+  })
+  return total;
 };
 
 const largeSphere = {
