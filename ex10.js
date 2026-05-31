2241afa63b77e18897bb4183f236b9a87d11e63b
Sae Nakatani <nakatanisae0218@gmail.com>
2026-05-31 16:12:12 -0700

    Exercise10 completed

--- ex10.js
+++ ex10.js
@@ -1,14 +1,26 @@
 /*In this exercise, we will be printing multiplication tables to the console.
 
 Multiplication Table
-We will be given a number as our input data. This number is the highest value of our multiplication table. Our job is to generate a multiplication table for the values from 1 to the provided number.
+We will be given a number as our input data. 
+This number is the highest value of our multiplication table. 
+Our job is to generate a multiplication table for the values from 1 to the provided number.
 
 Instruction
-Create a function named multiplicationTable that receives a number maxValue as input and creates a square multiplication table where maxValue is the largest value in the table.
+Create a function named multiplicationTable that receives a number maxValue as input 
+and creates a square multiplication table where maxValue is the largest value in the table.
 */
 
 const multiplicationTable = function (maxValue) {
   // Your code here
+  let result = '';
+  const NumberTable = maxValue
+  for(let i = 1; i <= maxValue; i++){
+    for(let j = 1; j <= maxValue; j++){
+      result += (i * j) + ' ';
+    }
+    result += '\n'
+  }
+  return result
 };
 
 console.log(multiplicationTable(1));
